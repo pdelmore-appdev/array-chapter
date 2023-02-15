500cfac88ad11a78269d58229c09f6fccc73047a
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-15 20:19:13 +0000

    assignment completed

--- array_element_square.rb
+++ array_element_square.rb
@@ -11,3 +11,7 @@ p "Enter at least 2 numbers, separated by spaces:"
 user_string = gets.chomp
 
 user_numbers = user_string.split
+
+second_to_last = user_numbers.at(-2).to_f
+
+p second_to_last ** 2
